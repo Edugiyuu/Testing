abd3d1db644c9b3968b03915c9f7f2d1497dc278
Eduardo Santos <edupaz077@gmail.com>
2024-03-06 22:59:44 -0300

    testing

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-// Import dependencies
-const fs = require("fs");
+ const fs = require("fs");
 const XLSX = require("xlsx");
-const jsontoxml = require("jsontoxml");
-
-const workbook = XLSX.readFile("Pasta1.xlsx")
-const worksheet = workbook.Sheets['Planilha1']
+/* 
+const arquivo = XLSX.readFile("Pasta1.xlsx")
+const worksheet = arquivo.Sheets['Planilha1']
 
 const arr = XLSX.utils.sheet_to_json(worksheet)
-console.log(arr);
+console.log(arr); */
+
+
+  const workbook = XLSX.readFile("ExtratoBancário.xlsx");
+
+  console.log("Planilhas encontradas:", workbook.SheetNames);
+
+ 
+  const primeiraPlanilha = workbook.Sheets[workbook.SheetNames[0]];
+  const dados = XLSX.utils.sheet_to_json(primeiraPlanilha);
+  console.log("Dados da primeira planilha:", dados[0]);
