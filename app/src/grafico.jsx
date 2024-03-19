d17ab063595bdc0f245dc85ebfe7e850f37cc6ba
Eduardo Santos <edupaz077@gmail.com>
2024-03-18 23:36:15 -0300

    Update grafico.jsx

--- app/src/grafico.jsx
+++ app/src/grafico.jsx
@@ -5,25 +5,29 @@ import { CartesianGrid,Legend, Line,LineChart,Tooltip,XAxis, YAxis} from "rechar
 
 
  const Grafico = () => {
-    const data = [
+    /* const data = [
         { ano: "2017", primeiroNumero: 32, segundoNumero: 37, },
         { ano: "2018", primeiroNumero: 42, segundoNumero: 42},
         { ano: "2019", primeiroNumero: 51, segundoNumero: 41},
         { ano: "2020", primeiroNumero: 60, segundoNumero: 37},
         { ano: "2021", primeiroNumero: 51, segundoNumero: 31},
         { ano: "2022", primeiroNumero: 95, segundoNumero: 44},
+      ]; */
+    const data = [
+        { primeiroNumero: 32, segundoNumero: 37, },
+        { primeiroNumero: 42, segundoNumero: 42},
+        { primeiroNumero: 51, segundoNumero: 41},
+        { primeiroNumero: 60, segundoNumero: 37},
+        {  primeiroNumero: 51, segundoNumero: 31},
+        {  primeiroNumero: 95, segundoNumero: 44},
       ];
   return (
     
-    <LineChart width={600} height={300} data={data}>
-    <Line type="monotone" dataKey="react" stroke="#2196F3" strokeWidth={3} />
-    <Line
-      type="monotone"
-      dataKey="primeiroNumero"
-      stroke="#F44236"
-      strokeWidth={3}
+    <LineChart width={1200} height={300} data={data}>
+    <Line type="monotone" dataKey="ano" stroke="#2196F3" strokeWidth={3} />
+    <Line type="monotone"dataKey="primeiroNumero"stroke="#F44236"strokeWidth={3}
     />
-    <Line type="monotone" dataKey="vue" stroke="#FFCA29" strokeWidth={3} />
+    <Line type="monotone" dataKey="segundoNumero" stroke="#FFCA29" strokeWidth={3} />
     <CartesianGrid stroke="#ccc" />
     <XAxis dataKey="name" />
     <YAxis />
